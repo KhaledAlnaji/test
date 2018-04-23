d662c256774f4fe6b4b6a85c63e9d3c801d5277b
GZ0002 <TeamCity_Agent167@nevs.com>
2018-04-23 14:20:45 +0200

    test

--- run_me.bat
+++ run_me.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files\MATLAB\R2016a\bin\matlab.exe" -nodesktop -nosplash -nodisplay -r /wait "cd 
-C:\repos\sandbox; run('sktipt.m'); exit;" -logfile "F:\matlablog.txt"
+"C:\Program Files\MATLAB\R2016a\bin\matlab.exe" -nodesktop -nosplash -nodisplay -r /wait "cd C:\repos\sandbox; run('sktipt.m'); exit; " -logfile "F:\matlablog.txt"
+
